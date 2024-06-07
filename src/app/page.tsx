b0dbe6b482d1f52ac4c1ff1aa9037b35ae1db1bc
GAP <g_patane@hotmail.com>
2024-06-07 01:25:39 -0300

    Test Commit 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to the custom app&nbsp;
+          Welcome to the custom app by GAP&nbsp;
           <code className="font-mono font-bold">
             {data.client ? data.client.givenName : data.company?.name}
           </code>
@@ -30,7 +30,7 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{' '} GAP
             <Image
               src="/copilot_icon.png"
               alt="Copilot Icon"
